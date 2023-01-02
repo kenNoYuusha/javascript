da8f6abed06cda0b792d4932aa6668f4a87f4472
Ren Amaki <jorge_luis1709@hotmail.com>
2023-01-01 19:49:18 -0600

    Abort controller

--- src/MediaPlayer.js
+++ src/MediaPlayer.js
@@ -6,7 +6,19 @@ function MediaPlayer(config) {
 }
 
 MediaPlayer.prototype._init_plugins = function(){
-  this.plugins.forEach( plugin => plugin.run(this))
+  const player = {
+    play: this.play,
+    pause: this.pause,
+    media: this.media,
+    get muted(){
+      return this.media.muted;
+    },
+    set muted(value){
+      this.media.muted = value;
+    }
+  }  
+  
+  this.plugins.forEach( plugin => plugin.run(player))
 }
 
 MediaPlayer.prototype.play = function () {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const video = document.querySelector("video");
 const button = document.querySelector("button");
 const muteButton = document.querySelector("#muteButton");
 
-const player = new MediaPlayer({ video });
+const player = new MediaPlayer({ video, plugins: [new AutoPlay()] });
 
 button.onclick = () => player.toogle();
 muteButton.onclick = () => player.toogleMute();
